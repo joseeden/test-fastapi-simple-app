8df741bc2a38e828d4f827e794109e732e455ff3
joseeden <josemanuelitoeden@gmail.com>
2024-12-06 22:28:50 +0800

    Added POST responses

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 def health_check():
     return {'status': 'Healthy'}
 
+@app.post("/healthy")
+def post_health_check(data: dict):
+    return {"status": "Healthy", "received": data}
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="localhost", port=5000, reload=True)
